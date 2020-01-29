e2c82048ce7226305a0d5e86fc841d424c62243a
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-01-28 23:00:03 -0300

    Comenzando con las rutas, query y params

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const server = express();
+
+// Query query = ?nome=reynaldo
+// Route params = /users/1
+// Request body = { "name": "Reynaldo", "email": "onlyreynaldo@icloud.com" }
+
+server.get('/teste', (req, res) => {
+   const nombre = req.query.nome;
+   return res.json({ message: `Retornando el nombre ${nombre}` });
+});
+
+server.get('/users/:id', (req, res) => {
+   const id = req.params.id;
+   return res.json({ message: `Buscando o usuario ${id}` });
+});
+
+// haciendo destructuracion
+server.get('/usersres/:id', (req, res) => {
+   const { id } = req.params;
+   return res.json({ message: `Buscando o usuario ${id} con restructuracion` });
+})
+
+server.listen(3000);
