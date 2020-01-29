575aea1d2247791dab3a1f71415875bb1a638dec
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-01-28 23:12:02 -0300

    Adicionando el campo con arrays, listar usuarios

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ server.get('/users/:id', (req, res) => {
 server.get('/usersres/:id', (req, res) => {
    const { id } = req.params;
    return res.json({ message: `Buscando o usuario ${id} con restructuracion` });
-})
+});
+
+const usuarios = ['Diego', 'Claudio', 'Victor'];
+
+server.get('/usuarios/:index', (req, res) => {
+   const { index } = req.params;
+   return res.json({ message: `Retornando el usuario ${usuarios[index]}` });
+});
 
 server.listen(3000);
